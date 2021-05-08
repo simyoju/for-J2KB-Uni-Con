a445c3df1c6a78b7aec48032227ef2d5a08d133a
simyoju <dawnofspring@daum.net>
2021-05-08 22:08:02 +0900

    Initial Commit

---
+++ Fridge_of_Everyone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fridge_of_Everyone
+//
+//  Created by simyo on 2021/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
