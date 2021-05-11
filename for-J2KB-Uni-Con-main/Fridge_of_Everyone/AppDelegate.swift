69a343a796a16fa58bcd42f6ad50de9813da7c81
swiftie1230 <63195670+swiftie1230@users.noreply.github.com>
2021-05-11 13:13:57 +0900

    Launch Screen

--- for-J2KB-Uni-Con-main/Fridge_of_Everyone/AppDelegate.swift
+++ for-J2KB-Uni-Con-main/Fridge_of_Everyone/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by simyo on 2021/05/08.
 //
 
+
 import UIKit
 
 @main
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
         return true
     }
 
