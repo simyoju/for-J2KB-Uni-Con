ef146597498e3813c0814676d17ef150b1d152b1
simyoju <dawnofspring@daum.net>
2021-05-12 22:30:00 +0900

    Initial Commit

---
+++ Fridge_of_Everyone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fridge_of_Everyone
+//
+//  Created by simyo on 2021/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
