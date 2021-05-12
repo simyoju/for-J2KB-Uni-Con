01c456384a83d10310eb686be86e6b220934cdbe
simyoju <dawnofspring@daum.net>
2021-05-13 00:42:01 +0900

    main.storyboard에 login, signup storyboard 옮겨놓음

--- Fridge_of_Everyone/ViewController.swift
+++ Fridge_of_Everyone/ViewController.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let mainVC : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
